28882c8372199d0c53062e960daec6a662044015
etretjakova <etretjakowa@yandex.ru>
2024-11-24 20:59:06 +0300

    initial commit

---
+++ src/main/java/ru/sky/coursework/_42/Coursework242Application.java
@@ -0,0 +1,13 @@
+package ru.sky.coursework._42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework242Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework242Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sky/coursework/_42/Coursework242ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sky.coursework._42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework242ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
